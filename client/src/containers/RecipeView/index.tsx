d32069c871fb8aad8ef1abe52b9dca2b7ffc7a52
peterzernia <peter.zernia@medigo.com>
2019-12-29 07:45:55 +0100

    added share buttons to recipe view

--- client/src/containers/RecipeView/index.tsx
+++ client/src/containers/RecipeView/index.tsx
@@ -4,6 +4,10 @@ import { getRecipe } from 'common/api'
 import Button from 'components/Button'
 import Loader from 'components/Loader'
 import { StateContext } from 'common/context'
+import Twitter from 'assets/twitter.svg'
+import Pintrest from 'assets/pinterest.svg'
+import Facebook from 'assets/facebook.svg'
+import Email from 'assets/email.svg'
 import './RecipeView.css'
 
 type RouteParams = {
@@ -35,7 +39,7 @@ export default function RecipeView(props: RouteComponentProps<RouteParams>): Rea
   if (loading) return <Loader />
 
   return (
-    <div>
+    <div className="recipeview">
       {
         recipe.author.id === state.user.id && (
           <div className="recipeview-edit">
@@ -45,10 +49,24 @@ export default function RecipeView(props: RouteComponentProps<RouteParams>): Rea
           </div>
         )
       }
-      <div>{recipe.title}</div>
-      <div>{recipe.author.username}</div>
-      <div>{recipe.ingredients}</div>
-      <div>{recipe.instructions}</div>
+      <h1>{recipe.title}</h1>
+      <h3>{recipe.author.username}</h3>
+      <div className="recipeview-ingredients">{recipe.ingredients}</div>
+      <div className="recipeview-instructions">{recipe.instructions}</div>
+      <div className="recipeview-share">
+        <a href={`https://www.facebook/share.php?u=${process.env.REACT_APP_API_URL}/recipes/${slug}/view`}>
+          <img height={16} src={Facebook} alt="facebook" />
+        </a>
+        <a href={`https://pinterest.com/pin/create/button/?url=${process.env.REACT_APP_API_URL}/recipes/${slug}/view`}>
+          <img height={16} src={Pintrest} alt="pinterest" />
+        </a>
+        <a href={`https://twitter.com/intent/tweet?text=${recipe.title} ${process.env.REACT_APP_API_URL}/recipes/${slug}/view`}>
+          <img height={16} src={Twitter} alt="twitter" />
+        </a>
+        <a href={`mailto:?Subject=${recipe.title}&body=Check out this recipe! ${process.env.REACT_APP_API_URL}/recipes/${slug}/view`}>
+          <img height={16} src={Email} alt="email" />
+        </a>
+      </div>
     </div>
   )
 }
