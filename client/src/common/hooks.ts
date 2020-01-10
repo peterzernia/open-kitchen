9458196e78caad776f94ec3199b910231ed6b552
peterzernia <peter.zernia@medigo.com>
2020-01-10 17:10:39 +0100

    added nav menu for small screens

--- client/src/common/hooks.ts
+++ client/src/common/hooks.ts
@@ -1,6 +1,10 @@
 import * as React from 'react'
 
-/* eslint import/prefer-default-export: 0 */
+type Dimensions = {
+  width: number;
+  height: number;
+}
+
 export const useDebounce = (value: string, delay: number): string => {
   const [debounced, setDebounced] = React.useState(value)
 
@@ -15,4 +19,33 @@ export const useDebounce = (value: string, delay: number): string => {
   }, [value, delay])
 
   return debounced
+}
+
+export const useWindowDimensions = (): Dimensions => {
+  const hasWindow = typeof window !== 'undefined'
+
+  const getWindowDimensions = React.useCallback((): Dimensions => {
+    const width = hasWindow ? window.innerWidth : null
+    const height = hasWindow ? window.innerHeight : null
+    return {
+      width,
+      height,
+    }
+  }, [hasWindow])
+
+  const [windowDimensions, setWindowDimensions] = React.useState(getWindowDimensions())
+
+  React.useEffect(() => {
+    if (hasWindow) {
+      const handleResize = (): void => {
+        setWindowDimensions(getWindowDimensions())
+      }
+
+      window.addEventListener('resize', handleResize)
+      return (): void => window.removeEventListener('resize', handleResize)
+    }
+    return null
+  }, [hasWindow, getWindowDimensions])
+
+  return windowDimensions
 }
--- client/src/components/Nav/index.tsx
+++ client/src/components/Nav/index.tsx
@@ -1,26 +1,69 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
+import Button from 'components/Button'
 import { StateContext } from 'common/context'
+import { useWindowDimensions } from 'common/hooks'
+import Menu from 'assets/icons/menu.svg'
+
 import './Nav.css'
 
 export default function Nav(): React.ReactElement {
+  const [open, setOpen] = React.useState(false)
   const state = React.useContext(StateContext)
+  const { width } = useWindowDimensions()
+  const isSmall = width < 450
 
   return (
-    <nav>
-      <Link className="nav-tab" to="/">open kitchen.</Link>
-      <div className="nav-right">
-        <Link className="nav-tab" to="/search">Search</Link>
+    <div>
+      <nav className="nav" style={{ height: isSmall && open && 150 }}>
+        <div className="nav-top">
+          <Link className="nav-tab" to="/">open kitchen.</Link>
+          {
+            !isSmall
+              ? (
+                <div className="nav-right">
+                  <Link className="nav-tab" to="/search">Search</Link>
+                  {
+                    state.authenticated
+                      && <Link className="nav-tab" to={`/recipes/${state.user.username}`}>Recipes</Link>
+                  }
+                  {
+                    state.authenticated
+                      ? <Link className="nav-tab" to="/logout">Logout</Link>
+                      : <Link className="nav-tab" to="/login">Login</Link>
+                  }
+                </div>
+              )
+              : (
+                <div className="nav-right">
+                  <Button onClick={(): void => setOpen(!open)} icon>
+                    <img src={Menu} alt="menu" />
+                  </Button>
+                </div>
+              )
+          }
+        </div>
         {
-          state.authenticated
-            && <Link className="nav-tab" to={`/recipes/${state.user.username}`}>Recipes</Link>
+          open && (
+            <div className="nav-bottom">
+              <div>
+                <Link className="nav-tab" to="/search">Search</Link>
+              </div>
+              {
+                state.authenticated
+                  && <div><Link className="nav-tab" to={`/recipes/${state.user.username}`}>Recipes</Link></div>
+              }
+              {
+                state.authenticated
+                  ? <div><Link className="nav-tab" to="/logout">Logout</Link></div>
+                  : <div><Link className="nav-tab" to="/login">Login</Link></div>
+              }
+            </div>
+          )
         }
-        {
-          state.authenticated
-            ? <Link className="nav-tab" to="/logout">Logout</Link>
-            : <Link className="nav-tab" to="/login">Login</Link>
-        }
-      </div>
-    </nav>
+      </nav>
+      {/* Nav uses absolite positioning for the menu. This div offsets content below the navbar. */}
+      <div className="nav-offset" />
+    </div>
   )
 }
