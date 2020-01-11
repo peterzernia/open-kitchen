b5c2e3a55412d18e8383ae42b3cde28eaebd9a92
peterzernia <peter.zernia@medigo.com>
2020-01-11 11:30:19 +0100

    added features section

--- client/src/containers/Home/index.tsx
+++ client/src/containers/Home/index.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react'
+import Save from 'assets/icons/save.svg'
+import SmartPhone from 'assets/icons/smartphone.svg'
+import Share from 'assets/icons/share.svg'
 
 import './Home.css'
 
@@ -8,6 +11,25 @@ export default function Home(): React.ReactElement {
       <header className="header">
         <span>open kitchen.</span>
       </header>
+
+      <div className="features">
+        <div className="feature my">
+          <img src={Save} className="feature-icon mx" alt="save" />
+          <h1 className="feature-title">Save</h1>
+          <h3>All of your recipes in one location!</h3>
+        </div>
+        <div className="feature my">
+          <img src={SmartPhone} className="feature-icon mx" alt="smartphone" />
+          <h1 className="feature-title">Access</h1>
+          <h3>All of your recipes on any device!</h3>
+        </div>
+        <div className="feature my">
+          <img src={Share} className="feature-icon mx" alt="share" />
+          <h1 className="feature-title">Share</h1>
+          <h3>Your recipes anytime, anywhere!</h3>
+        </div>
+      </div>
+
     </div>
   )
 }
