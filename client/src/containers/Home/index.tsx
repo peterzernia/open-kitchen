4fefa34b0e4a800c05fa6577c461edd0036213eb
peterzernia <peter.zernia@medigo.com>
2020-01-11 11:55:19 +0100

    added testimonials

--- client/src/containers/Home/index.tsx
+++ client/src/containers/Home/index.tsx
@@ -2,6 +2,9 @@ import * as React from 'react'
 import Save from 'assets/icons/save.svg'
 import SmartPhone from 'assets/icons/smartphone.svg'
 import Share from 'assets/icons/share.svg'
+import TestimonialOne from 'assets/images/testimonial-1.jpg'
+import TestimonialTwo from 'assets/images/testimonial-2.jpg'
+import TestimonialThree from 'assets/images/testimonial-3.jpg'
 
 import './Home.css'
 
@@ -30,6 +33,30 @@ export default function Home(): React.ReactElement {
         </div>
       </div>
 
+      <div className="testimonials">
+        <div className="testimonial my">
+          <img src={TestimonialOne} className="testimonial-img" alt="one" />
+          <h3 className="testimonial-name">Fred S.</h3>
+          <p>
+            &quot;I hated how scattered my recipes used to be.
+            Now I can keep track of them in one location!&quot;
+          </p>
+        </div>
+        <div className="testimonial my">
+          <img src={TestimonialTwo} className="testimonial-img" alt="one" />
+          <h3 className="testimonial-name">Pretty B.</h3>
+          <p>
+            &quot;I really like how many chicken recipes are on here.&quot;
+          </p>
+        </div>
+        <div className="testimonial my">
+          <img src={TestimonialThree} className="testimonial-img" alt="one" />
+          <h3 className="testimonial-name">Sarah W.</h3>
+          <p>
+            &quot;Sharing recipes hase never been so easy!.&quot;
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
